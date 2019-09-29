9777728674b89b2f17e89a46524ffaa5f5a49ae0
Tomek Mularczyk <tomaszmularczyk89@gmail.com>
2019-09-29 16:57:31 +0200

    part I

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Contact from "./Contact";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Contact</div>
+        <div>Use form below to contact us:</div>
+      </div>
+    );
+  }
+}
